7ac0b29c97a86021635e01010f778473b797eee6
vastamaa <vsztom@gmail.com>
2022-09-13 20:31:58 +0200

    Finished implementing services.

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() selectedRecipe: Recipe;
-  constructor() { }
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onAddToShoppingList() {
+    this.recipeService.addIngrediensToShoppingList(
+      this.selectedRecipe.ingredients
+    );
   }
-
 }
